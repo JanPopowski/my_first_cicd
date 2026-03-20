5fbb95e1e9c09aaebeb367d95aadb2fb32445a8d
Jan Popowski <janpopowski@c68-228.wmi.amu.edu.pl>
2026-03-20 09:02:25 +0100

    add: github actions tests

--- test_main.py
+++ test_main.py
@@ -14,6 +14,6 @@ def test_add_positive_numbers():
     assert response.json() == {"result": 5}
 
 def test_add_negative_numbers():
-    response = client.get("/add/-2/1")
+    response = client.get("/add/-2/12")
     assert response.status_code == 200
     assert response.json() == {"result": -1}
